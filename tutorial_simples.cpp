aff24eca2a6f916c6e27cce9ea805f3cd315df4a
Jhon Marcos <kenzhait@gmail.com>
2022-12-04 14:48:47 -0800

    primeros pasos

---
+++ tutorial_simples.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+#include<stdio.h> //conservar compatibilidad con c
+
+using namespace std;
+
+const int sumar_hasta = 1; //puedes colocar variables fuera
+
+//variables
+int main()
+
+
+{
+    //variables
+    int primer_variable = 64;
+    float a;
+    a = 1.2;
+    float b = primer_variable + a; //suma estas dos variables
+    const int sumar_hasta = 1;
+    // constantes
+
+
+    cout<<primer_variable<<endl;
+    cout<<a<<endl; //imprime la variable a -1.2
+    cout <<"imprime en pantalla cosas"<<endl;
+
+    //salto de linea
+
+    cout<<sumar_hasta<<endl;
+
+    char palabra;
+    cout<<"ingresa un valor"<<endl;
+    cin>>palabra; //es cin es equivalente a input en python
+
+    system("pause");
+    //operadores aritmeticos +-*/=%
+
+    return 0;//retorna cero
+
+
+}
+
