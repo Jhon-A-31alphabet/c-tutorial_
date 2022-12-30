49e8d288e44da7bb6d1a281476bde783ddf735aa
Jhon Marcos Araujo <108773254+Jhon-A-31alphabet@users.noreply.github.com>
2022-12-30 13:50:54 -0800

    Update excepciones.cpp

--- excepciones.cpp
+++ excepciones.cpp
@@ -17,3 +17,16 @@ int main()
   catch(const int x){cerr<<"error >> "<<endl;} //cerr is to print a error message...
   //if parameter of cath is a char x. you have to write like this const char *x if throw has a string;
  }
+
+void otheway(){
+  int a=1;
+  int n;
+  cin>>n;
+  try{
+    if(n==0){
+      
+      throw 0;}
+  }
+  catch(const int x){cerr<<"error >> "<<endl;} //cerr is to print a error message...
+  //the value of x is 0 cause throw. you can add whaterever thing int cath{.......}
+}
