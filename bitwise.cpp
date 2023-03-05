ec9d9e8d3fac9c37ce11dab3afc46ccdc5069b73
Jhon Marcos Araujo <108773254+Jhon-A-31alphabet@users.noreply.github.com>
2023-03-05 12:45:12 -0400

    Add files via upload

---
+++ bitwise.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<stdio.h>
+int main(){
+
+    //  ~ invierte los bits:  ejemplo 000:  111.     100:011 -> el nombre es complemento a uno.
+    //  >> desplaza los bits a la derecha.
+    //  << desplaza los bits a la izquierda.
+
+    //  & operador and logico, binario.  compara ambos operandos bit a bit. si un bit de cada operando es 1 devuelve 1.
+    // de lo contrario devuelve cero.
+
+    // ^ xor = parecido al and logico. con la diferencia de si un bit de cada operando es igual devuelve 0. Y si son diferentes devuelve 1.
+
+    // | or = Este operador binario tiene un funcionamiento parecido a los anteriores (AND y XOR),
+    //salvo que en este caso el resultado es 1 si
+    //alguno de ellos está a 1. En caso contrario devuelve 0 (ver ejemplo).
+
+
+    int a = 10;
+    int b =20;
+
+    std::cout<<(a|b)<<std::endl;
+    std::cout<<(a^b)<<std::endl;
+    std::cout<<(a>>b)<<std::endl;
+    std::cout<<(a<<b)<<std::endl;
+    std::cout<<(a&b)<<std::endl;
+
+return 0;
+}
